edc8d5f1904db508a5d475eae6ae524e281426b3
Vladimir <vladimir.rostok96@gmail.com>
2024-07-10 23:33:33 +0300

    Clean up new generated smart contract code

--- world-debt-solution/src/world_debt_solution.rs
+++ world-debt-solution/src/world_debt_solution.rs
@@ -1,14 +1,5 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use multiversx_sc::imports::*;
 
-/// An empty contract. To be used as a template when starting a new contract from scratch.
-#[multiversx_sc::contract]
-pub trait WorldDebtSolution {
-    #[init]
-    fn init(&self) {}
-
-    #[upgrade]
-    fn upgrade(&self) {}
-}
+    
